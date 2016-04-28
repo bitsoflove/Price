4bc71305845e91ef2fbb1b8e1472bbccc8723af7
Stijn Tilleman <stijn@bitsoflove.be>
2016-04-28 15:20:59 +0200

    Added Price getter/setter to indicate cents workflow

--- Entities/Price.php
+++ Entities/Price.php
@@ -8,32 +8,49 @@ use Modules\Price\Entities\Unit;
 
 class Price extends Model
 {
+    /**
+     * @var string
+     */
+    protected $table = 'prices';
 
     /**
-     * Generated
+     * @var array
      */
+    protected $fillable = [
+        'id',
+        'price_type_id',
+        'price',
+        'currency_id',
+        'unit_id'
+    ];
 
-    protected $table = 'prices';
-    protected $fillable = ['id', 'price_type_id', 'price', 'currency_id', 'unit_id'];
-
-    
-
-
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function currency()
     {
         return $this->belongsTo(Currency::class, 'currency_id', 'id');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function priceType()
     {
         return $this->belongsTo(PriceType::class, 'price_type_id', 'id');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function unit()
     {
         return $this->belongsTo(Unit::class, 'unit_id', 'id');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function productVersions()
     {
         return $this->belongsToMany(config('asgard.product.config.entities.product-version.class',
@@ -44,13 +61,36 @@ class Price extends Model
         );
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function productVersionPrices()
     {
         return $this->hasMany(ProductVersionPrice::class, 'price_id', 'id');
     }
 
+    /**
+     * Retrieve the cents value into an actual price value
+     *
+     * @return float
+     */
+    public function getPriceAttribute(){
+        return $this->attributes['price'] / 100;
+    }
 
+    /**
+     * Transform a given price into it's cents value
+     *
+     * @param float $price
+     */
+    public function setPriceAttribute($price){
+        $this->attributes['price'] = $price * 100;
+    }
 
+    /**
+     * @param array $attributes
+     * @return bool|int
+     */
     public function update(array $attributes = [])
     {
         $res = parent::update($attributes);
@@ -58,6 +98,10 @@ class Price extends Model
         return $res;
     }
 
+    /**
+     * @param array $attributes
+     * @return static
+     */
     public static function create(array $attributes = [])
     {
         $res = parent::create($attributes);
@@ -67,6 +111,9 @@ class Price extends Model
 
     /**
      * Sync many-to-many relationships
+     * 
+     * @param Model $model
+     * @param array $attributes
      */
     private static function sync($model, array $attributes = [])
     {
